8c2e867d350a803659d5701bf36927541cad7967
julienc91 <julien.chaumont91@hotmail.fr>
2013-01-12 14:36:59 +0100

    Màj de la traduction fr sur l'éditeur (à voir comment les faire marcher sur les .ui)

--- Editor/GridEditor/GridEditor_fr.ts
+++ Editor/GridEditor/GridEditor_fr.ts
@@ -2,32 +2,97 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="fr_FR">
 <context>
+    <name>AudioSync</name>
+    <message>
+        <location filename="AudioSync.cpp" line="14"/>
+        <source>Beginning</source>
+        <translation>Début</translation>
+    </message>
+    <message>
+        <location filename="AudioSync.cpp" line="15"/>
+        <source>End</source>
+        <translation>Fin</translation>
+    </message>
+    <message>
+        <location filename="AudioSync.cpp" line="16"/>
+        <source>Bar</source>
+        <translation>Mesure</translation>
+    </message>
+    <message>
+        <location filename="AudioSync.cpp" line="21"/>
+        <source>Compute</source>
+        <translation>Calculer</translation>
+    </message>
+</context>
+<context>
+    <name>AudioWindow</name>
+    <message>
+        <location filename="AudioWindow.cpp" line="7"/>
+        <source>Audio file</source>
+        <translation>Fichier audio</translation>
+    </message>
+    <message>
+        <location filename="AudioWindow.cpp" line="11"/>
+        <source>Browse</source>
+        <translation>Parcourir</translation>
+    </message>
+    <message>
+        <location filename="AudioWindow.cpp" line="100"/>
+        <source>Open a file</source>
+        <translation>Ouvrir un fichier</translation>
+    </message>
+    <message>
+        <location filename="AudioWindow.cpp" line="100"/>
+        <source>Music (*.mp3 *.ogg *.wma *.wav)</source>
+        <translation>Musique (*.mp3 *.ogg *.wma *.wav)</translation>
+    </message>
+</context>
+<context>
     <name>ChordTableWidget</name>
     <message>
-        <location filename="ChordTableWidget.cpp" line="35"/>
-        <location filename="ChordTableWidget.cpp" line="59"/>
+        <location filename="ChordTableWidget.cpp" line="38"/>
         <source>Annotation</source>
         <translation>Annotation</translation>
     </message>
     <message>
-        <location filename="ChordTableWidget.cpp" line="320"/>
+        <location filename="ChordTableWidget.cpp" line="63"/>
+        <source>Properties</source>
+        <translation>Propriétés</translation>
+    </message>
+    <message>
+        <location filename="ChordTableWidget.cpp" line="64"/>
+        <source>Play from here</source>
+        <translation>Jouer à partir d&apos;ici</translation>
+    </message>
+    <message>
+        <location filename="ChordTableWidget.cpp" line="247"/>
         <source>Are you sure you want to delete lines </source>
         <translation>Êtes-vous sûr de vouloir supprimer les lignes </translation>
     </message>
     <message>
-        <location filename="ChordTableWidget.cpp" line="328"/>
+        <location filename="ChordTableWidget.cpp" line="255"/>
         <source>Deleting lines</source>
         <translation>Suppression de lignes</translation>
     </message>
     <message>
-        <location filename="ChordTableWidget.cpp" line="354"/>
+        <location filename="ChordTableWidget.cpp" line="281"/>
         <source>Are you sure you want to delete columns </source>
         <translation>Êtes-vous sûr de vouloir supprimer les colonnes </translation>
     </message>
     <message>
-        <location filename="ChordTableWidget.cpp" line="362"/>
+        <location filename="ChordTableWidget.cpp" line="289"/>
         <source>Deleting columns</source>
         <translation>Suppression de colonnes</translation>
+    </message>
+    <message>
+        <location filename="ChordTableWidget.cpp" line="438"/>
+        <source>Before doing stupid things</source>
+        <translation>Avant de faire des bêtises</translation>
+    </message>
+    <message>
+        <location filename="ChordTableWidget.cpp" line="438"/>
+        <source>Some timers have already been set manually. Do you want to reset them too?</source>
+        <translation>Certains marqueurs ont été réglés manuellement. Voulez-vous les recalculer eux-aussi?</translation>
     </message>
 </context>
 <context>
@@ -41,23 +106,32 @@
 <context>
     <name>EditionSelector</name>
     <message>
-        <location filename="EditionSelector.cpp" line="13"/>
         <source>Two edit modes are available, you just have to choose one to begin the adventure!</source>
-        <translation>Deux modes d&apos;édition sont disponibles, il n&apos;y a qu&apos;à choisir lequel vous voulez pour commencer l&apos;aventure!</translation>
+        <translation type="obsolete">Deux modes d&apos;édition sont disponibles, il n&apos;y a qu&apos;à choisir lequel vous voulez pour commencer l&apos;aventure!</translation>
     </message>
     <message>
-        <location filename="EditionSelector.cpp" line="23"/>
-        <location filename="EditionSelector.cpp" line="34"/>
+        <location filename="EditionSelector.cpp" line="14"/>
+        <source>What do you want to do today?</source>
+        <translation>Que souhaitez-vous faire aujourd&apos;hui?</translation>
+    </message>
+    <message>
+        <location filename="EditionSelector.cpp" line="15"/>
+        <source>Show this screen at startup</source>
+        <translation>Afficher cet fenêtre au démarrage</translation>
+    </message>
+    <message>
+        <location filename="EditionSelector.cpp" line="25"/>
+        <location filename="EditionSelector.cpp" line="36"/>
         <source>Begin</source>
         <translation>Commencer</translation>
     </message>
     <message>
-        <location filename="EditionSelector.cpp" line="41"/>
+        <location filename="EditionSelector.cpp" line="43"/>
         <source>Manual Editor</source>
         <translation>Editeur manuel</translation>
     </message>
     <message>
-        <location filename="EditionSelector.cpp" line="42"/>
+        <location filename="EditionSelector.cpp" line="44"/>
         <source>Assisted Editor</source>
         <translation>Editeur assisté</translation>
     </message>
@@ -65,193 +139,286 @@
 <context>
     <name>GridEditor</name>
     <message>
-        <location filename="GridEditor.cpp" line="61"/>
+        <location filename="GridEditor.cpp" line="80"/>
         <source>&amp;File</source>
         <translation>&amp;Fichier</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="62"/>
+        <location filename="GridEditor.cpp" line="81"/>
         <source>&amp;Edit</source>
         <translation>&amp;Edition</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="63"/>
+        <location filename="GridEditor.cpp" line="82"/>
         <source>&amp;Options</source>
         <translation>&amp;Options</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="64"/>
+        <location filename="GridEditor.cpp" line="83"/>
         <source>&amp;About</source>
         <translation>&amp;A propos</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="73"/>
+        <location filename="GridEditor.cpp" line="92"/>
         <source>&amp;Quit</source>
         <translation>&amp;Quitter</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="74"/>
+        <location filename="GridEditor.cpp" line="93"/>
         <source>About</source>
         <translation>A propos</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="75"/>
+        <location filename="GridEditor.cpp" line="94"/>
         <source>&amp;New</source>
-        <translation>$Nouveau</translation>
+        <translation>&amp;Nouveau</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="76"/>
+        <location filename="GridEditor.cpp" line="95"/>
         <source>&amp;Save</source>
         <translation>Enregi&amp;strer</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="77"/>
+        <location filename="GridEditor.cpp" line="96"/>
         <source>&amp;Open</source>
         <translation>&amp;Ouvrir</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="78"/>
+        <location filename="GridEditor.cpp" line="97"/>
         <source>Add row</source>
         <translation>Ajouter une ligne</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="79"/>
+        <location filename="GridEditor.cpp" line="98"/>
         <source>Delete row</source>
         <translation>Supprimer une ligne</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="80"/>
+        <location filename="GridEditor.cpp" line="99"/>
         <source>Add column</source>
         <translation>Ajouter une colonne</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="81"/>
+        <location filename="GridEditor.cpp" line="100"/>
         <source>Delete column</source>
         <translation>Supprimer une colonne</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="82"/>
+        <location filename="GridEditor.cpp" line="101"/>
         <source>&amp;Copy down</source>
         <translation>&amp;Copier</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="83"/>
+        <location filename="GridEditor.cpp" line="102"/>
         <source>Rename</source>
         <translation>Renommer</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="129"/>
+        <location filename="GridEditor.cpp" line="103"/>
+        <source>Audio</source>
+        <translation>Audio</translation>
+    </message>
+    <message>
+        <location filename="GridEditor.cpp" line="104"/>
+        <source>Properties</source>
+        <translation>Propriétés</translation>
+    </message>
+    <message>
+        <location filename="GridEditor.cpp" line="152"/>
         <source>Tool bar</source>
         <translation>Barre d&apos;outils</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="215"/>
+        <location filename="GridEditor.cpp" line="249"/>
+        <source>The document has been modified.</source>
+        <translation>Le document a été modifié.</translation>
+    </message>
+    <message>
+        <location filename="GridEditor.cpp" line="250"/>
+        <source>Do you want to save your changes?</source>
+        <translation>Voulez-vous enregistrer vos modifications?</translation>
+    </message>
+    <message>
+        <location filename="GridEditor.cpp" line="332"/>
+        <source>XML Files (*.xml)</source>
+        <translation>Fichiers XML (*.xml)</translation>
+    </message>
+    <message>
         <source>Open chords grid</source>
-        <translation>Ouvrir une grille d&apos;accords</translation>
+        <translation type="obsolete">Ouvrir une grille d&apos;accords</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="215"/>
         <source>xgrid Files (*.xgrid)</source>
-        <translation>Fichiers xgrid (*.xgrid)</translation>
+        <translation type="obsolete">Fichiers xgrid (*.xgrid)</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="245"/>
-        <location filename="GridEditor.cpp" line="306"/>
         <source>Grid name</source>
-        <translation>Nom de la grille</translation>
+        <translation type="obsolete">Nom de la grille</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="245"/>
         <source>Name the current grid to save it.
 
  Name:</source>
-        <translation>Donnez un nom à la grille pour l&apos;enregistrer.
+        <translation type="obsolete">Donnez un nom à la grille pour l&apos;enregistrer.
 
  Nom:</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="250"/>
         <source>Warning</source>
-        <translation>Attention</translation>
+        <translation type="obsolete">Attention</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="251"/>
         <source>You have to enter a name to continue.</source>
-        <translation>Vous devez indiquer un nom pour continuer.</translation>
+        <translation type="obsolete">Vous devez indiquer un nom pour continuer.</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="262"/>
         <source>Grid saved</source>
-        <translation>Grille enregistrée</translation>
+        <translation type="obsolete">Grille enregistrée</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="263"/>
         <source>The chords grid is saved at ./</source>
-        <translation>La grille d&apos;accords a été enregistrée sous ./</translation>
+        <translation type="obsolete">La grille d&apos;accords a été enregistrée sous ./</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="277"/>
         <source>Column number:</source>
-        <translation>Nombre de colonnes:</translation>
+        <translation type="obsolete">Nombre de colonnes:</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="279"/>
         <source>Warning:
 Unsaved modifications on current grid will be lost.
 
 Column number:</source>
-        <translation>Attention:
+        <translation type="obsolete">Attention:
 Toute modification non sauvegardée sera perdue.
 
 Nombre de colonnes:</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="280"/>
         <source>New grid</source>
-        <translation>Nouvelle grille</translation>
+        <translation type="obsolete">Nouvelle grille</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="306"/>
         <source>Name:</source>
-        <translation>Nom:</translation>
+        <translation type="obsolete">Nom:</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="322"/>
+        <location filename="GridEditor.cpp" line="313"/>
         <source>So sorry...</source>
         <translation>Désolé...</translation>
     </message>
     <message>
-        <location filename="GridEditor.cpp" line="322"/>
+        <location filename="GridEditor.cpp" line="313"/>
         <source>This function has not been implemented yet, but come back soon!</source>
         <translation>Cette fonction n&apos;est pas encore disponible, mais revenez vite!</translation>
     </message>
 </context>
 <context>
+    <name>NewGridDialog</name>
+    <message>
+        <location filename="NewGridDialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+    <message>
+        <location filename="NewGridDialog.ui" line="45"/>
+        <source>Artist</source>
+        <translation>Artiste</translation>
+    </message>
+    <message>
+        <location filename="NewGridDialog.ui" line="81"/>
+        <source>Track name</source>
+        <translation>Nom du morceau</translation>
+    </message>
+    <message>
+        <location filename="NewGridDialog.ui" line="97"/>
+        <source>Chords per bar</source>
+        <translation>Accords par mesure</translation>
+    </message>
+    <message>
+        <location filename="NewGridDialog.ui" line="126"/>
+        <source>Columns</source>
+        <translation>Colonnes</translation>
+    </message>
+    <message>
+        <location filename="NewGridDialog.ui" line="174"/>
+        <source>Lines</source>
+        <translation>Lignes</translation>
+    </message>
+</context>
+<context>
+    <name>PartSetter</name>
+    <message>
+        <location filename="PartSetter.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+    <message>
+        <location filename="PartSetter.ui" line="54"/>
+        <source>Beginning of a new part</source>
+        <translation>Début d&apos;une nouvelle partie</translation>
+    </message>
+    <message>
+        <location filename="PartSetter.ui" line="84"/>
+        <source>Beginning</source>
+        <translation>Début</translation>
+    </message>
+    <message>
+        <location filename="PartSetter.ui" line="100"/>
+        <source>Part name</source>
+        <translation>Nom de la partie</translation>
+    </message>
+    <message>
+        <location filename="PartSetter.ui" line="127"/>
+        <source>Cell properties</source>
+        <translation>Propriétés de la case</translation>
+    </message>
+</context>
+<context>
     <name>SimpleMusicPlayer</name>
     <message>
-        <location filename="SimpleMusicPlayer.cpp" line="7"/>
         <source>Browse</source>
-        <translation>Parcourir</translation>
+        <translation type="obsolete">Parcourir</translation>
     </message>
     <message>
-        <location filename="SimpleMusicPlayer.cpp" line="58"/>
         <source>Open a file</source>
-        <translation>Ouvrir un fichier</translation>
+        <translation type="obsolete">Ouvrir un fichier</translation>
     </message>
     <message>
-        <location filename="SimpleMusicPlayer.cpp" line="58"/>
         <source>Music (*.mp3 *.ogg *.wma *.wav)</source>
-        <translation>Musique (*.mp3 *.ogg *.wma *.wav)</translation>
+        <translation type="obsolete">Musique (*.mp3 *.ogg *.wma *.wav)</translation>
     </message>
     <message>
-        <location filename="SimpleMusicPlayer.cpp" line="61"/>
+        <location filename="SimpleMusicPlayer.cpp" line="90"/>
         <source>So sorry...</source>
         <translation>Désolé...</translation>
     </message>
     <message>
-        <location filename="SimpleMusicPlayer.cpp" line="61"/>
+        <location filename="SimpleMusicPlayer.cpp" line="90"/>
         <source>There was an error while trying to play the file...</source>
         <translation>Il y a eu une erreur lors de la lecture du fichier...</translation>
+    </message>
+</context>
+<context>
+    <name>TrackProperties</name>
+    <message>
+        <location filename="TrackProperties.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+    <message>
+        <location filename="TrackProperties.ui" line="65"/>
+        <source>Track name</source>
+        <translation>Nom du morceau</translation>
+    </message>
+    <message>
+        <location filename="TrackProperties.ui" line="81"/>
+        <source>Artist</source>
+        <translation>Artiste</translation>
+    </message>
+    <message>
+        <location filename="TrackProperties.ui" line="113"/>
+        <source>Chords per bar</source>
+        <translation>Accords par mesure</translation>
     </message>
 </context>
 </TS>
