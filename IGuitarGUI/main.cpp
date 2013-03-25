1284c5c5e3e7f413a7d2a794b8d359b025962950
pakokrew <pakokrew@gmail.com>
2013-03-25 12:17:39 +0100

    Ajout icones (pas codé pour l'editeur) Correction polices AMeliorations graphiques

--- IGuitarGUI/main.cpp
+++ IGuitarGUI/main.cpp
@@ -9,10 +9,13 @@ int main(int argc, char *argv[]){
   translator.load(":/translations/IGuitarGUI_fr");
   app.installTranslator(&translator);
 
-  QFontDatabase::addApplicationFont(":/fonts/Roboto-Regular.ttf");
-  QFontDatabase::addApplicationFont(":/fonts/Roboto-Black.ttf");
-  QFontDatabase::addApplicationFont(":/fonts/Roboto-BoldCondensed.ttf");
-  QFontDatabase::addApplicationFont(":/fonts/Roboto-Italic.ttf");
+  QFontDatabase::addApplicationFont("qrc:/fonts/Roboto-Regular.ttf");
+  QFontDatabase::addApplicationFont("qrc:/fonts/Roboto-Black.ttf");
+  QFontDatabase::addApplicationFont("qrc:/fonts/Roboto-BoldCondensed.ttf");
+  QFontDatabase::addApplicationFont("qrc:/fonts/Roboto-Italic.ttf");
+
+  QIcon appIcon(":images/playerIcon.png");
+  QApplication::setWindowIcon(appIcon);
 
   QRect screenGeo = QApplication::desktop()->screenGeometry();
 
